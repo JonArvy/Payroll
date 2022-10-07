49d8ee15b9a54af81145814d870256cf80ddd974
Arvy Enriquez <jonarvy11@gmail.com>
2022-10-07 09:41:06 +0800

    Credentials

---
+++ src/main/java/Controller/Additional/CredentialsValidationController.java
@@ -0,0 +1,29 @@
+package Controller.Additional;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CredentialsValidationController {
+
+    @FXML
+    private TextField emp_id;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private CheckBox show_password;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
